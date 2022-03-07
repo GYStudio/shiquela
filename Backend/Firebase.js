41bcd2efbea6906c7155de2d45fe942a88fb2324
Gerwn Yonas <gerwynyonas@gmail.com>
2022-03-07 14:19:13 +0300

    Firebase Details

---
+++ Backend/Firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAwuX67qBNVnZI_1NIf0ggkF-Sn7Z-EwBM",
+  authDomain: "shiquela-2b436.firebaseapp.com",
+  projectId: "shiquela-2b436",
+  storageBucket: "shiquela-2b436.appspot.com",
+  messagingSenderId: "102117094579",
+  appId: "1:102117094579:web:b350d7e409b99cad7aacc6",
+  measurementId: "G-EX79GQQXEQ"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
